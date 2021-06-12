2f5b63046afd93b6d18cb300dc5b2c2b3f89ca6b
Andrew Vanderstarre <aavanderstarre@live.ca>
2021-06-12 11:09:29 -0400

    Votes are now only allowed once every day per user, needs to be optimized

--- src/components/leaderboard/LeaderboardRow.js
+++ src/components/leaderboard/LeaderboardRow.js
@@ -1,9 +1,13 @@
 import React, { useEffect, useState } from 'react'
 import './styles.css'
 import Button from 'react-bootstrap/Button'
+import Alert from 'react-bootstrap/Alert'
 import {auth, firestore, firebase} from '../config/fbConfig';
+import {useAuth} from '../../contexts/AuthContext.js'
 
 const LeaderboardRow = ({sortByVotes, id, name, age, marketcap, votes,index}) => {
+    const userInformation = useAuth();
+
     const [totalVotes, setVotes] = useState(votes)
     useEffect(() =>{
         updateVotes();
@@ -18,8 +22,61 @@ const LeaderboardRow = ({sortByVotes, id, name, age, marketcap, votes,index}) =>
             // The document probably doesn't exist.
             console.error("Error updating document: ", error);
         });
-        
     }
+
+    // Updates time when user voted on database
+    const vote = async() => {
+        // Checks if user is logged in
+        if (userInformation.currentUser == null){
+            console.log("You must be logged in to vote")
+            return;
+        }
+
+        // Gets date of last from database
+        const info = firestore.collection("users").doc(userInformation.currentUser.uid);
+        const doc = await info.get();
+        var lastVoteDate = doc.data().tokens.[id];
+
+        // gets todays date
+        var today = new Date();
+        var dd = String(today.getDate()).padStart(2, '0');
+        var mm = String(today.getMonth() + 1).padStart(2, '0'); 
+        var yyyy = today.getFullYear();
+        today = mm + '/' + dd + '/' + yyyy;
+
+        if (doc != null){ // Need to check if tokens.id exists here *************
+            // Save lastVoteDate as Date variable to allow for date math
+            // lastVoteDate = new Date(doc.data().tokens.[id]);
+        }else{
+            // First time user has voted for token
+            await firestore.collection("users").doc(userInformation.currentUser.uid).set({
+                tokens: { [id]: today}
+            });
+        }
+
+        if (lastVoteDate){
+            // Allow vote if user has not voted today
+            if (lastVoteDate != today){
+                await firestore.collection("users").doc(userInformation.currentUser.uid).set({
+                    // Edits last vote date
+                    tokens: { [id]: today}
+                });
+                setVotes(totalVotes+1);
+            }else{
+                console.log("You can only vote once every 24 hours");
+            }
+
+        }else{
+            // If user has not voted yet, create a new entry for the token
+            await firestore.collection("users").doc(userInformation.currentUser.uid).set({
+                tokens: { [id]: today}
+            }, {merge: true}
+            );
+
+            setVotes(totalVotes+1);
+        }
+    }
+
     return (
              <tr>
                 <td>{index+1}</td>
@@ -27,7 +84,7 @@ const LeaderboardRow = ({sortByVotes, id, name, age, marketcap, votes,index}) =>
                 <td>{marketcap}</td>
                 <td> {age}                </td>
                 <td> 
-                    <Button onClick={() => {setVotes(totalVotes+1); sortByVotes();}} className="voteButton">
+                    <Button onClick={() => { vote(); sortByVotes();}} className="voteButton">
                         {totalVotes}
                     </Button>
                 </td>
