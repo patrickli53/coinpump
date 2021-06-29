286c2ab8ec50ee55f8d6024e8185dbd1bf727aad
patrickli53 <patrick.li7h@gmail.com>
2021-06-28 22:33:39 -0400

    limited loading on render

--- src/components/leaderboard/index.js
+++ src/components/leaderboard/index.js
@@ -5,13 +5,19 @@ import Alert from 'react-bootstrap/Alert'
 
 import './styles.css'
 import LeaderboardRow from './LeaderboardRow';
+import { Button } from 'react-bootstrap';
 
 const Leaderboard = ({sortMethod, promoted, searchText}) => {
     const [leaderboard, setLeaderboard] = useState([])
-
+    const [loadAll, setLoadAll] = useState(false)
     useEffect(() => {
         fetchData();
     }, []);
+    useEffect(() => {
+        if(searchText != ""){
+            setLoadAll(true)
+        }
+    }, [searchText]);
     
     async function fetchData() {
         console.log(promoted)
@@ -57,39 +63,63 @@ const Leaderboard = ({sortMethod, promoted, searchText}) => {
 
 
     const renderLeaderboardRows = () => {
-        return leaderboard.filter(r=>{
-            if(!searchText.trim()){
-                return true
-            }
-            
-            return r.Name.toLowerCase().includes(searchText.toLowerCase())
-            
-        }).map((doc, index)=>{
-            // Adds coin only if its approved by admin
-                return (
-                    
-                    // <LeaderboardRow 
-                    //     alert={alert} 
-                    //     id={doc.id} 
-                    //     index={index} 
-                    //     name={doc.Name} 
-                    //     marketcap={doc.MarketCap} 
-                    //     age={((Date.now() - doc.Date.toDate())/(1000*24*60*60)).toFixed(0)} 
-                    //     votes={doc.Votes}  weeklyVotes={doc.WeeklyVotes}
-                    //     logo = {doc.Logo}
-                    //     contractAddress = {doc.ContractAddress}
-                    // />
-
-                    <LeaderboardRow 
-                        doc={doc}
-                        index={index} 
-                        sortMethod={sortMethod}
-                    />
-                )
-        })
+        if(searchText == "" || !loadAll){
+            return leaderboard.slice(0,20).map((doc, index)=>{
+                // Adds coin only if its approved by admin
+                    return (
+                        <LeaderboardRow 
+                            doc={doc}
+                            index={index} 
+                            sortMethod={sortMethod}
+                        />
+                    )
+            })
+        }
+        else{
+            return leaderboard.filter(r=>{
+                if(!searchText.trim()){
+                    return true
+                }
+                
+                return r.Name.toLowerCase().includes(searchText.toLowerCase())
+                
+            }).map((doc, index)=>{
+                // Adds coin only if its approved by admin
+                    return (
+                        
+                        // <LeaderboardRow 
+                        //     alert={alert} 
+                        //     id={doc.id} 
+                        //     index={index} 
+                        //     name={doc.Name} 
+                        //     marketcap={doc.MarketCap} 
+                        //     age={((Date.now() - doc.Date.toDate())/(1000*24*60*60)).toFixed(0)} 
+                        //     votes={doc.Votes}  weeklyVotes={doc.WeeklyVotes}
+                        //     logo = {doc.Logo}
+                        //     contractAddress = {doc.ContractAddress}
+                        // />
+    
+                        <LeaderboardRow 
+                            doc={doc}
+                            index={index} 
+                            sortMethod={sortMethod}
+                        />
+                    )
+            })
+        }
     }
 
-
+    const renderLoadButton = () => {
+        if (!promoted && !loadAll){
+            return <Button onClick={()=>setLoadAll(true)}>Load all coins</Button>
+        }
+        else if(promoted){
+            
+        }
+        else{
+            return "All coins loaded."
+        }
+    }
     return (
         <div>   
             <Table striped borderless hover>
@@ -109,6 +139,9 @@ const Leaderboard = ({sortMethod, promoted, searchText}) => {
                     {renderLeaderboardRows()}
                 </tbody>
             </Table>
+            <div>
+                {renderLoadButton()}
+            </div>
         </div>
     )
 }
